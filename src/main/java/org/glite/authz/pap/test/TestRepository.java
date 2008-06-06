81d71c5ba8703adc72be648227174189152c6485
Alberto Forti <alberto.forti@cnaf.infn.it>
2008-06-06 08:56:33 +0000

    Added comments

--- src/main/java/org/glite/authz/pap/test/TestRepository.java
+++ src/main/java/org/glite/authz/pap/test/TestRepository.java
@@ -27,18 +27,21 @@ public class TestRepository {
 		if (!rootPolicySetDAO.existsRoot()) {
 				rootPolicySetDAO.createRoot();
 		}
-		
+
+		// Create a PAP PolicySet
 		PolicySet localPAPPolicySet = policySetBuilder.buildFromFile(papPolicySetTemplatePath);
 		localPAPPolicySet.setId("LocalPolicySet");
 		if (!rootPolicySetDAO.existsPAP(localPAPPolicySet.getId())) {
 			rootPolicySetDAO.createPAPAsFirst(localPAPPolicySet);
 		}
 		
+		// Insert PolicySet in the PAP
 		PolicySet examplePolicySet = policySetBuilder.buildFromFile(papPolicySetTemplatePath);
 		examplePolicySet.setId("example_policyset_01");
 		PolicySetDAO policySetDAO = daoFactory.getPolicySetDAO();
 		policySetDAO.store(localPAPPolicySet.getId(), examplePolicySet);
 		
+		// Insert Policy in the PAP
 		PolicyDAO policyDAO = daoFactory.getPolicyDAO();
 		Policy examplePolicy = policyBuilder.buildFromFile(policyTemplate);
 		examplePolicy.setId("example_policy_01");
