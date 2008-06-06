ff9f657010762ebbdba1e757eb0ef99ca1c5c9e7
Alberto Forti <alberto.forti@cnaf.infn.it>
2008-06-06 08:11:04 +0000

    Refatctoring

--- src/main/java/org/glite/authz/pap/test/TestRepository.java
+++ src/main/java/org/glite/authz/pap/test/TestRepository.java
@@ -1,9 +1,10 @@
 package org.glite.authz.pap.test;
 
+import org.glite.authz.pap.common.RepositoryConfiguration;
 import org.glite.authz.pap.common.xacml.Policy;
-import org.glite.authz.pap.common.xacml.PolicyImpl;
+import org.glite.authz.pap.common.xacml.PolicyBuilder;
 import org.glite.authz.pap.common.xacml.PolicySet;
-import org.glite.authz.pap.common.xacml.PolicySetImpl;
+import org.glite.authz.pap.common.xacml.PolicySetBuilder;
 import org.glite.authz.pap.repository.dao.DAOFactory;
 import org.glite.authz.pap.repository.dao.PolicyDAO;
 import org.glite.authz.pap.repository.dao.PolicySetDAO;
@@ -12,9 +13,13 @@ import org.glite.authz.pap.repository.dao.RootPolicySetDAO;
 public class TestRepository {
 	
 	private static final String papPolicySetTemplatePath = "files/RootPAPPolicySetTemplate.xml";
-	private static final String policyTemplate = "files/PolicyTemplate.xml"; 
+	private static final String policyTemplate = "files/PolicyTemplate.xml";
+	private static PolicySetBuilder policySetBuilder;
+	private static PolicyBuilder policyBuilder;
 
 	public static void main(String[] args) {
+		
+		policySetBuilder = RepositoryConfiguration.getPolicySetBuilder();
 
 		DAOFactory daoFactory = DAOFactory.getDAOFactory();
 		RootPolicySetDAO rootPolicySetDAO = daoFactory.getRootPolicySetDAO();
@@ -23,19 +28,19 @@ public class TestRepository {
 				rootPolicySetDAO.createRoot();
 		}
 		
-		PolicySet localPAPPolicySet = new PolicySetImpl(papPolicySetTemplatePath);
+		PolicySet localPAPPolicySet = policySetBuilder.buildFromFile(papPolicySetTemplatePath);
 		localPAPPolicySet.setId("LocalPolicySet");
 		if (!rootPolicySetDAO.existsPAP(localPAPPolicySet.getId())) {
 			rootPolicySetDAO.createPAPAsFirst(localPAPPolicySet);
 		}
 		
-		PolicySet examplePolicySet = new PolicySetImpl(papPolicySetTemplatePath);
+		PolicySet examplePolicySet = policySetBuilder.buildFromFile(papPolicySetTemplatePath);
 		examplePolicySet.setId("example_policyset_01");
 		PolicySetDAO policySetDAO = daoFactory.getPolicySetDAO();
 		policySetDAO.store(localPAPPolicySet.getId(), examplePolicySet);
 		
 		PolicyDAO policyDAO = daoFactory.getPolicyDAO();
-		Policy examplePolicy = new PolicyImpl(policyTemplate);
+		Policy examplePolicy = policyBuilder.buildFromFile(policyTemplate);
 		examplePolicy.setId("example_policy_01");
 		policyDAO.store(localPAPPolicySet.getId(), examplePolicy);
 		
