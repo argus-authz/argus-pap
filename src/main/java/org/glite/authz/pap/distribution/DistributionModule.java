9dc69fb14ae6544527ff3b9006b624ed4d8de569
Alberto Forti <alberto.forti@cnaf.infn.it>
2008-07-14 08:13:47 +0000

    Saving work done on the train.

--- src/main/java/org/glite/authz/pap/distribution/DistributionModule.java
+++ src/main/java/org/glite/authz/pap/distribution/DistributionModule.java
@@ -5,19 +5,12 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.glite.authz.pap.common.PAP;
-import org.glite.authz.pap.common.utils.xacml.PolicyHelper;
-import org.glite.authz.pap.common.utils.xacml.PolicySetHelper;
-import org.glite.authz.pap.provisioning.client.ProvisioningServiceClient;
-import org.glite.authz.pap.provisioning.client.ProvisioningServiceClientFactory;
-import org.glite.authz.pap.provisioning.client.ProvisioningServicePortType;
 import org.glite.authz.pap.repository.PAPContainer;
 import org.glite.authz.pap.repository.PAPManager;
 import org.glite.authz.pap.repository.RepositoryManager;
-import org.opensaml.saml2.core.Response;
 import org.opensaml.xacml.XACMLObject;
 import org.opensaml.xacml.policy.PolicySetType;
 import org.opensaml.xacml.policy.PolicyType;
-import org.opensaml.xacml.profile.saml.XACMLPolicyQueryType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,7 +18,6 @@ public class DistributionModule extends Thread {
 
 	private static final Logger log = LoggerFactory.getLogger(DistributionModule.class);
 	private static DistributionModule instance = new DistributionModule();
-	private final ProvisioningServiceClient client;
 
 	public static DistributionModule getInstance() {
 		return instance;
@@ -38,28 +30,35 @@ public class DistributionModule extends Thread {
 	    DistributionConfigurationParser conf = DistributionConfigurationParser.getInstance();
 	    sleepTime = conf.getPollingInterval();
 	    remotePAPs = conf.getRemotePAPs();
-	    
-	    ProvisioningServiceClientFactory factory = ProvisioningServiceClientFactory.getProvisioningServiceClientFactory();
-        client = factory.createPolicyProvisioningServiceClient();
-        
 	}
 
-	public void end() {
-		log.info("Shutting down distribution module...");
-		this.interrupt();
-		while (this.isAlive());
-		log.info("Distribution module stopped");
+	public List<XACMLObject> getPoliciesFromPAP(PAP remotePAP) {
+		List<XACMLObject> papPolicies = new LinkedList<XACMLObject>();
+		
+		PAPClient client = new PAPClient(remotePAP.getEndpoint());
+		papPolicies = client.getLocalPolicies();
+		
+		// Fake policy generation... here the client gets the policies from the remote PAP
+//		papPolicies.add(PolicySetHelper.buildWithAnyTarget(remotePAP.getPapId(), PolicySetHelper.COMB_ALG_ORDERED_DENY_OVERRIDS));
+//		for (int i=0; i<10; i++) {
+//			papPolicies.add(PolicySetHelper.buildWithAnyTarget(remotePAP.getPapId() + "_Ex_" + i, PolicySetHelper.COMB_ALG_ORDERED_DENY_OVERRIDS));
+//			papPolicies.add(PolicyHelper.buildWithAnyTarget(remotePAP.getPapId() + "_Ex_" + i, PolicyHelper.RULE_COMBALG_DENY_OVERRIDS));
+//		}
+		
+		log.info("Retrieved policies from: " + remotePAP.getDn());
+		return papPolicies;
 	}
 	
 	public void run() {
 		try {
+		    sleep(sleepTime);
 			while (!this.isInterrupted()) {
-				for (PAP remotePAP : remotePAPs) {
+				for (PAP pap : remotePAPs) {
 					if (this.isInterrupted()) {
 						break;
 					}
-					List<XACMLObject> papTree = getPoliciesFromPAP(remotePAP);
-					storePAPPolicies(remotePAP, papTree);
+					List<XACMLObject> papPolicies = getPoliciesFromPAP(pap);
+					storePAPPolicies(pap, papPolicies);
 				}
 				sleep(sleepTime);
 			}
@@ -67,31 +66,17 @@ public class DistributionModule extends Thread {
 		}
 	}
 
-	private List<XACMLObject> getPoliciesFromPAP(PAP remotePAP) {
-		List<XACMLObject> papPolicies = new LinkedList<XACMLObject>();
-		
-		ProvisioningServicePortType port = client.getProvisioningServicePortType("http://example.org/ProvisioningService");
-		
-		XACMLPolicyQueryType query;
-		
-		//Response response = port.xacmlPolicyQuery(xacmlPolicyQueryType);
-		
-		// Fake policy generation... here the client gets the policies from the remote PAP
-		papPolicies.add(PolicySetHelper.buildWithAnyTarget(remotePAP.getPapId(), PolicySetHelper.COMB_ALG_ORDERED_DENY_OVERRIDS));
-		for (int i=0; i<10; i++) {
-			papPolicies.add(PolicySetHelper.buildWithAnyTarget(remotePAP.getPapId() + "_Ex_" + i, PolicySetHelper.COMB_ALG_ORDERED_DENY_OVERRIDS));
-			papPolicies.add(PolicyHelper.buildWithAnyTarget(remotePAP.getPapId() + "_Ex_" + i, PolicyHelper.RULE_COMBALG_DENY_OVERRIDS));
-		}
-		
-		log.info("Retrieved policies from: " + remotePAP.getDn());
-		return papPolicies;
+	public void startDistributionModule() {
+	    this.start();
 	}
 
-	private void init() {
-	    
-		    
+	public void stopDistributionModule() {
+		log.info("Shutting down distribution module...");
+		this.interrupt();
+		while (this.isAlive());
+		log.info("Distribution module stopped");
 	}
-
+	
 	private void storePAPPolicies(PAP remotePAP, List<XACMLObject> papPolicies) {
 		String papId = remotePAP.getDn();
 		if (papPolicies.isEmpty()) {
@@ -99,6 +84,8 @@ public class DistributionModule extends Thread {
 		}
 		PAPContainer papContainer = null;
 		PAPManager papManager = RepositoryManager.getPAPManager();
+		remotePAP.setPapId(remotePAP.getPapId() + "_distribution");
+		remotePAP.setDn(remotePAP.getDn() + "_distribution");
 		if (!papManager.exists(remotePAP)) {
 			papContainer = papManager.create(remotePAP);
 		} else {
