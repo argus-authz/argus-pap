d29b45baf03c6da07070abdc87509e2e37c55ca4
Alberto Forti <alberto.forti@cnaf.infn.it>
2008-07-16 09:47:20 +0000

    Fixed to retrieve stuff from configuration.

--- src/main/java/org/glite/authz/pap/repository/dao/filesystem/FileSystemRepositoryManager.java
+++ src/main/java/org/glite/authz/pap/repository/dao/filesystem/FileSystemRepositoryManager.java
@@ -2,6 +2,8 @@ package org.glite.authz.pap.repository.dao.filesystem;
 
 import java.io.File;
 
+import org.glite.authz.pap.common.PAP;
+import org.glite.authz.pap.common.PAPConfiguration;
 import org.glite.authz.pap.repository.RepositoryManager;
 import org.glite.authz.pap.repository.exceptions.RepositoryException;
 import org.slf4j.Logger;
@@ -10,12 +12,17 @@ import org.slf4j.LoggerFactory;
 public class FileSystemRepositoryManager extends RepositoryManager {
 	
 	private static final Logger log = LoggerFactory.getLogger( FileSystemRepositoryManager.class );
-	protected static final String fileSystemDatabaseDir = "/tmp/paprep";
+	protected static final String fileSystemDatabaseDir;
 	private static final String rootPolicySetId = "Root";
-	private static final String localPAPId = "Local";
+	private static final String localPAPId;
 	private static final String policySetFileNamePrefix = "PolicySet_";
 	private static final String policyFileNamePrefix = "Policy_";
 	private static final String xacmlFileNameExtension = ".xml";
+	
+	static {
+	    fileSystemDatabaseDir = PAPConfiguration.instance().getPAPRepositoryDir();
+	    localPAPId = PAP.localPAPId;
+	}
 	
 	protected void initialize() {
 		log.info("Initializing filesystem repository...");
@@ -28,6 +35,7 @@ public class FileSystemRepositoryManager extends RepositoryManager {
 		if (!(rootDir.canRead() && rootDir.canWrite())) {
 			throw new RepositoryException("Permission denied for root DB dir: " + rootDir.getAbsolutePath());
 		}
+		log.info("Repository root directory is: " + rootDir.getAbsolutePath());
 	}
 
 	public static String getFileSystemDatabaseDir() {
