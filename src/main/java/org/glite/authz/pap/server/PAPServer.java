4f9848486b0673c64a3f6f861482a4fa1525af8a
Andrea Ceccanti <andrea.ceccanti@cnaf.infn.it>
2008-12-02 14:59:00 +0000

    Refactored the PAP server code a bit to allow for nonstandard conf and repo directories.

--- src/main/java/org/glite/authz/pap/server/PAPServer.java
+++ src/main/java/org/glite/authz/pap/server/PAPServer.java
@@ -23,123 +23,191 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class PAPServer {
-    
-    private static final Logger log = LoggerFactory.getLogger( PAPServer.class );
-    
-    private static final String STANDALONE_STANZA = "standalone-configuration";
-    
-    private static final String TRUSTMANAGER_STANZA = STANDALONE_STANZA+":trustmanager"; 
-    
-    
-    private static void configureConnector(Connector connector){
-        
-        connector.setHost( getString( "host", "localhost" ));
-        connector.setPort( getInt( "port", 4554));
 
-        // Add more sophisticated configuration of the connector below
-        
+    final class PAPDefaults {
+
+        static final int PORT = 4554;
+
+        static final int MAX_REQUEST_QUEUE_SIZE = 0;
+
+        static final int MAX_CONNECTIONS = 0;
+
     }
-    
+
+    private static final Logger log = LoggerFactory.getLogger( PAPServer.class );
+
+    private static final String STANDALONE_STANZA = "standalone-configuration";
+
+    private static final String TRUSTMANAGER_STANZA = STANDALONE_STANZA
+            + ":trustmanager";
+
+    private static final String CONF_DIR_OPTION_NAME = "conf-dir";
+
+    private static final String REPO_DIR_OPTION_NAME = "repo-dir";
+
     public static void main( String[] args ) {
 
-        // Initialize PAP configuration
-        PAPConfiguration.initialize();
-        
-        int maxRequestQueueSize  = getInt("max_request_queue_size", 0);
-        int maxConnections = getInt("max_connections", 30 );
-        
-        Server httpServer = new Server(getInt( "port", 4554));
-        
-        // Are these needed for the PAP?
-        httpServer.setSendServerVersion(false);
-        httpServer.setSendDateHeader(false);
-        
-        BlockingQueue<Runnable> requestQueue;
-        
-        if (maxRequestQueueSize < 1) {
-            requestQueue = new LinkedBlockingQueue<Runnable>();
-        } else {
-            requestQueue = new ArrayBlockingQueue<Runnable>(maxRequestQueueSize);
-        }
-        
-        ThreadPool threadPool = new ThreadPool(5, maxConnections, 60, TimeUnit.SECONDS, requestQueue);
-        httpServer.setThreadPool(threadPool);
-        
-        TrustManagerSocketConnector connector = new TrustManagerSocketConnector(getTrustmanagerConfiguration());
-        
-        // TrustManagerSelectChannelConnector connector = new TrustManagerSelectChannelConnector(getTrustmanagerConfiguration());
-        // SslSelectChannelConnector connector = new SslSelectChannelConnector();
-        
-        configureConnector( connector );
-        
-        httpServer.setConnectors( new Connector[]{connector} );
-        
-        Context servletContext = new Context(httpServer,"/", false, false);
-        
-        FilterHolder securityFilter = new FilterHolder(new SecurityContextFilter());
-        securityFilter.setName( "Security context filter" );
-        servletContext.addFilter( securityFilter, "/*", 0);
-        
-        servletContext.addEventListener( new PAPContextListener() );
-        
-        ServletHolder axisServlet = new ServletHolder(new AxisServlet());
-        axisServlet.setName( "Axis servlet" );
-        servletContext.addServlet( axisServlet, "/services/*");
-        
-        ServletHolder testServlet = new ServletHolder(new TestServlet());
-        testServlet.setName( "Test servlet" );
-        servletContext.addServlet( testServlet, "/test" );
-        
+        new PAPServer( args );
+
+    }
+
+    protected String papConfigurationDir;
+
+    protected String papRepositoryDir;
+
+    protected Server httpServer;
+
+    public PAPServer( String[] args ) {
+
+        parseOptions( args );
+
+        PAPConfiguration.initialize( papConfigurationDir, papRepositoryDir );
+
+        configureHttpServer();
+
+        configureServlets();
+
         try {
             httpServer.start();
-            log.info("PAP started and listening on {}:{}", connector.getHost(), connector.getPort());
             httpServer.join();
-            
-        } catch (Exception e) {
-            log.info("PAP encountered an error that could not be dealt with, shutting down.", e);
-            
+
+        } catch ( Exception e ) {
+            log
+                    .info(
+                            "PAP encountered an error that could not be dealt with, shutting down.",
+                            e );
+
             try {
                 httpServer.stop();
-            
+
             } catch ( Exception e1 ) {
                 // Just ignore this
             }
         }
-        
+
     }
-    
-    
-    private static String getString(String key, String defaultValue){
-        
+
+    private void configureHttpServer() {
+
+        httpServer = new Server( getInt( "port", PAPDefaults.PORT ) );
+
+        int maxRequestQueueSize = getInt( "max_request_queue_size",
+                PAPDefaults.MAX_REQUEST_QUEUE_SIZE );
+        int maxConnections = getInt( "max_connections",
+                PAPDefaults.MAX_CONNECTIONS );
+
+        httpServer.setSendServerVersion( false );
+        httpServer.setSendDateHeader( false );
+
+        BlockingQueue <Runnable> requestQueue;
+
+        if ( maxRequestQueueSize < 1 ) {
+            requestQueue = new LinkedBlockingQueue <Runnable>();
+        } else {
+            requestQueue = new ArrayBlockingQueue <Runnable>(
+                    maxRequestQueueSize );
+        }
+
+        ThreadPool threadPool = new ThreadPool( 5, maxConnections, 60,
+                TimeUnit.SECONDS, requestQueue );
+        httpServer.setThreadPool( threadPool );
+
+        TrustManagerSocketConnector connector = new TrustManagerSocketConnector(
+                getTrustmanagerConfiguration() );
+
+        connector.setHost( getString( "host", "localhost" ) );
+        connector.setPort( getInt( "port", PAPDefaults.PORT ) );
+
+        httpServer.setConnectors( new Connector[] { connector } );
+
+    }
+
+    private void configureServlets() {
+
+        Context servletContext = new Context( httpServer, "/", false, false );
+
+        FilterHolder securityFilter = new FilterHolder(
+                new SecurityContextFilter() );
+        securityFilter.setName( "Security context filter" );
+        servletContext.addFilter( securityFilter, "/*", 0 );
+
+        servletContext.addEventListener( new PAPContextListener() );
+
+        ServletHolder axisServlet = new ServletHolder( new AxisServlet() );
+        axisServlet.setName( "Axis servlet" );
+        servletContext.addServlet( axisServlet, "/services/*" );
+
+        ServletHolder testServlet = new ServletHolder( new TestServlet() );
+        testServlet.setName( "Test servlet" );
+        servletContext.addServlet( testServlet, "/test" );
+
+    }
+
+    private int getInt( String key, int defaultValue ) {
+
         PAPConfiguration conf = PAPConfiguration.instance();
-        return conf.getString( STANDALONE_STANZA+"."+key, defaultValue ); 
-        
+        return conf.getInt( STANDALONE_STANZA + "." + key, defaultValue );
     }
-    
-    
-    private static int getInt(String key, int defaultValue){
-        
+
+    private String getString( String key, String defaultValue ) {
+
         PAPConfiguration conf = PAPConfiguration.instance();
-        return conf.getInt( STANDALONE_STANZA+"."+key, defaultValue ); 
+        return conf.getString( STANDALONE_STANZA + "." + key, defaultValue );
+
     }
-    
-    
-    private static Properties getTrustmanagerConfiguration(){
-        
-                      
+
+    private Properties getTrustmanagerConfiguration() {
+
         PAPConfiguration papConf = PAPConfiguration.instance();
-        
+
         Properties tmProps = new Properties();
-        
+
         Configuration tmConf = papConf.subset( TRUSTMANAGER_STANZA );
-        Iterator<String> tmConfKeys = tmConf.getKeys();
-        
-        while(tmConfKeys.hasNext()){
+        Iterator <String> tmConfKeys = tmConf.getKeys();
+
+        while ( tmConfKeys.hasNext() ) {
             String key = tmConfKeys.next();
             tmProps.put( key, tmConf.getProperty( key ) );
         }
-            
+
         return tmProps;
+
+    }
+
+    protected void parseOptions( String[] args ) {
+
+        if ( args.length > 0 ) {
+
+            int currentArg = 0;
+
+            while ( currentArg < args.length ) {
+
+                if ( !args[currentArg].startsWith( "--" ) ) {
+                    usage();
+                } else if ( args[currentArg].equalsIgnoreCase( "--"
+                        + CONF_DIR_OPTION_NAME ) ) {
+                    papConfigurationDir = args[currentArg + 1];
+                    log.info( "Starting PAP with configuration dir: {}", papConfigurationDir );
+                    currentArg = currentArg + 2;
+                } else if ( args[currentArg].equalsIgnoreCase( "--"
+                        + REPO_DIR_OPTION_NAME ) ) {
+                    papRepositoryDir = args[currentArg + 1];
+                    log.info( "Starting PAP with repo dir: {}", papRepositoryDir );
+                    currentArg = currentArg + 2;
+                } else
+                    usage();
+
+            }
+
+        }
+    }
+
+    private void usage() {
+
+        String usage = "PAPServer [--" + CONF_DIR_OPTION_NAME
+                + " <confDir>] [--" + REPO_DIR_OPTION_NAME + " <repoDir>]";
         
+        System.out.println(usage);
+        System.exit( 0 );
     }
 }
