ab12ebafcf6a2218d59af4dedca2eb9e5b072610
Alberto Forti <alberto.forti@cnaf.infn.it>
2009-02-11 14:52:54 +0000

    Added ProvisioningService client

--- src/main/java/org/glite/authz/pap/client/ServiceClient.java
+++ src/main/java/org/glite/authz/pap/client/ServiceClient.java
@@ -3,6 +3,7 @@ package org.glite.authz.pap.client;
 import org.glite.authz.pap.services.authz_management.axis_skeletons.PAPAuthorizationManagement;
 import org.glite.authz.pap.services.highlevel_policy_management.axis_skeletons.HighLevelPolicyManagement;
 import org.glite.authz.pap.services.pap_management.axis_skeletons.PAPManagement;
+import org.glite.authz.pap.services.provisioning.axis_skeletons.Provisioning;
 import org.glite.authz.pap.services.xacml_policy_management.axis_skeletons.XACMLPolicyManagement;
 
 public interface ServiceClient {
@@ -20,6 +21,8 @@ public interface ServiceClient {
     public HighLevelPolicyManagement getHighLevelPolicyManagementService(String url);
     
     public PAPAuthorizationManagement getPAPAuthorizationManagementService(String url);
+    
+    public Provisioning getProvisioningService(String url);
     
     public String getTargetEndpoint();
 
