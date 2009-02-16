ade1693d5c988cb4f81ced3519871ec027209013
Alberto Forti <alberto.forti@cnaf.infn.it>
2009-02-16 09:43:26 +0000

    Added operations for setOrder() and getOrder()

--- src/main/java/org/glite/authz/pap/services/PAPManagementService.java
+++ src/main/java/org/glite/authz/pap/services/PAPManagementService.java
@@ -3,14 +3,15 @@ package org.glite.authz.pap.services;
 import java.rmi.RemoteException;
 
 import org.glite.authz.pap.authz.papmanagement.AddTrustedPAPOperation;
+import org.glite.authz.pap.authz.papmanagement.GetOrderOperation;
 import org.glite.authz.pap.authz.papmanagement.GetTrustedPAPOperation;
 import org.glite.authz.pap.authz.papmanagement.ListTrustedPAPsOperation;
 import org.glite.authz.pap.authz.papmanagement.RefreshPolicyCacheOperation;
 import org.glite.authz.pap.authz.papmanagement.RemoveTrustedPAPOperation;
+import org.glite.authz.pap.authz.papmanagement.SetOrderOperation;
 import org.glite.authz.pap.authz.papmanagement.TrustedPAPExistsOperation;
 import org.glite.authz.pap.authz.papmanagement.UpdateTrustedPAPOperation;
 import org.glite.authz.pap.common.PAP;
-import org.glite.authz.pap.distribution.PAPManager;
 import org.glite.authz.pap.distribution.PAPManagerException;
 import org.glite.authz.pap.services.pap_management.axis_skeletons.PAPData;
 import org.glite.authz.pap.services.pap_management.axis_skeletons.PAPManagement;
@@ -51,9 +52,7 @@ public class PAPManagementService implements PAPManagement {
 	public String[] getOrder() throws RemoteException {
 		try {
 
-			String[] papOrderArray = null;
-			papOrderArray = PAPManager.getInstance().getRemotePAPConfigurationOrder();
-			return papOrderArray;
+			return GetOrderOperation.instance().execute();
 
 		} catch (RuntimeException e) {
 			ServiceClassExceptionManager.log(log, e);
@@ -117,8 +116,7 @@ public class PAPManagementService implements PAPManagement {
 	public boolean setOrder(String[] aliasArray) throws RemoteException {
 		try {
 
-			PAPManager.getInstance().setPAPOrder(aliasArray);
-			return true;
+			return SetOrderOperation.instance(aliasArray).execute();
 
 		} catch (RuntimeException e) {
 			ServiceClassExceptionManager.log(log, e);
