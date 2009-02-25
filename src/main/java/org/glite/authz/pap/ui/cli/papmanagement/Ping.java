af54f63fd25975c939c95692304553bdbf5c4b8a
Alberto Forti <alberto.forti@cnaf.infn.it>
2009-02-25 14:56:18 +0000

    Reverting to previous version (rev 453)

--- src/main/java/org/glite/authz/pap/ui/cli/papmanagement/Ping.java
+++ src/main/java/org/glite/authz/pap/ui/cli/papmanagement/Ping.java
@@ -1,19 +1,11 @@
 package org.glite.authz.pap.ui.cli.papmanagement;
 
 import java.rmi.RemoteException;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.glite.authz.pap.common.xacml.utils.XMLObjectHelper;
-import org.glite.authz.pap.common.xacml.wizard.AttributeWizard;
-import org.glite.authz.pap.common.xacml.wizard.PolicySetWizard;
-import org.glite.authz.pap.common.xacml.wizard.PolicyWizard;
 import org.glite.authz.pap.ui.cli.CLIException;
-import org.glite.authz.pap.ui.cli.policymanagement.XACMLPolicyCLIUtils;
-import org.opensaml.xacml.policy.EffectType;
 
 public class Ping extends PAPManagementCLI {
 	
@@ -30,46 +22,16 @@ public class Ping extends PAPManagementCLI {
 	protected int executeCommand(CommandLine commandLine)
 			throws CLIException, ParseException, RemoteException {
 	    
-	    XACMLPolicyCLIUtils.initOpenSAML();
-	    
-	    AttributeWizard attribute = new AttributeWizard("resource_uri", "pippo");
-	    
-	    PolicySetWizard psw = new PolicySetWizard(attribute);
-	    
-	    System.out.println("---------------");
-	    System.out.println(psw.toFormattedString(true));
-	    System.out.println("---------------");
-	    
-	    //System.out.println(XMLObjectHelper.toString(psw.getXACML()));
-
-	    List<AttributeWizard> ruleTarget1 = new LinkedList<AttributeWizard>();
-	    ruleTarget1.add(new AttributeWizard("dn", "/DN=test_00"));
-	    
-	    List<AttributeWizard> ruleTarget2 = new LinkedList<AttributeWizard>();
-        ruleTarget2.add(new AttributeWizard("dn", "/DN=test_02"));
-	    
-	    PolicyWizard pw = new PolicyWizard(new AttributeWizard("action", "job_submission"));
-	    pw.addRule(ruleTarget1, EffectType.Deny);
-	    pw.addRule(ruleTarget2, EffectType.Permit);
-	    
-	    System.out.println(pw.toFormattedString(true));
-	    
-	    System.out.println("---------------");
-	    psw.addPolicy(pw);
-	    System.out.println(psw.toFormattedString(true));
-	    
-	    return ExitStatus.SUCCESS.ordinal();
+		String[] args = commandLine.getArgs();
 		
-//		String[] args = commandLine.getArgs();
-//		
-//		if (args.length > 1)
-//			throw new ParseException("Wrong number of arguments");
-//		
-//		String papVersion = papMgmtClient.ping();
-//		
-//		System.out.println("PAP successfully contacted: version=" + papVersion);
-//		
-//		return ExitStatus.SUCCESS.ordinal();
+		if (args.length > 1)
+			throw new ParseException("Wrong number of arguments");
+		
+		String papVersion = papMgmtClient.ping();
+		
+		System.out.println("PAP successfully contacted: version=" + papVersion);
+		
+		return ExitStatus.SUCCESS.ordinal();
   	}
 
 	@Override
