2327a3c586f3262ebc71619061a6522f5aaadc6c
Alberto Forti <alberto.forti@cnaf.infn.it>
2009-03-14 15:23:55 +0000

    Added some info to the help message

--- src/main/java/org/glite/authz/pap/ui/cli/policymanagement/BanAttribute.java
+++ src/main/java/org/glite/authz/pap/ui/cli/policymanagement/BanAttribute.java
@@ -12,8 +12,10 @@ public class BanAttribute extends PolicyManagementCLI {
 
     private static String[] COMMAND_NAME_VALUES_DN = { "ban-user", "bu" };
     private static String[] COMMAND_NAME_VALUES_FQAN = { "ban-fqan", "bf" };
-    private static String DESCRIPTION_DN = "Ban a DN.";
-    private static String DESCRIPTION_FQAN = "Ban an FQAN.";
+    private static String DESCRIPTION_DN = "Ban a DN. By default the DN is bannen for all resources and actions (i.e. \"*\"). "
+            + "Specify a resource or an action (using the options listed below) if you want to reduce the scope of the ban.";
+    private static String DESCRIPTION_FQAN = "Ban a FQAN. By default the FQAN is bannen for all resources and actions (i.e. \"*\"). "
+            + "Specify a resource or an action (using the options listed below) if you want to reduce the scope of the ban.";
     private static String OPT_ACTION = "a";
     private static String OPT_ACTION_DESCRIPTION = "Specify an action value.";
     private static String OPT_ACTION_LONG = "action";
@@ -45,13 +47,22 @@ public class BanAttribute extends PolicyManagementCLI {
     protected Options defineCommandOptions() {
         Options options = new Options();
 
-        options.addOption(OptionBuilder.hasArg(false).withDescription("Set the policy as public (default)").withLongOpt(
-                OPT_PUBLIC_LONG).create());
-        options.addOption(OptionBuilder.hasArg(false).withDescription("Set the policy as private (it won't be distributed)")
-                .withLongOpt(OPT_PRIVATE_LONG).create());
-        options.addOption(OptionBuilder.hasArg().withDescription(OPT_ACTION_DESCRIPTION).withLongOpt(OPT_ACTION_LONG).create(OPT_ACTION));
-        options.addOption(OptionBuilder.hasArg().withDescription(OPT_RESOURCE_DESCRIPTION).withLongOpt(OPT_RESOURCE_LONG).create(
-                OPT_RESOURCE));
+        options.addOption(OptionBuilder.hasArg(false)
+                                       .withDescription("Set the policy as public (default)")
+                                       .withLongOpt(OPT_PUBLIC_LONG)
+                                       .create());
+        options.addOption(OptionBuilder.hasArg(false)
+                                       .withDescription("Set the policy as private (it won't be distributed)")
+                                       .withLongOpt(OPT_PRIVATE_LONG)
+                                       .create());
+        options.addOption(OptionBuilder.hasArg()
+                                       .withDescription(OPT_ACTION_DESCRIPTION)
+                                       .withLongOpt(OPT_ACTION_LONG)
+                                       .create(OPT_ACTION));
+        options.addOption(OptionBuilder.hasArg()
+                                       .withDescription(OPT_RESOURCE_DESCRIPTION)
+                                       .withLongOpt(OPT_RESOURCE_LONG)
+                                       .create(OPT_RESOURCE));
 
         return options;
     }
@@ -72,13 +83,13 @@ public class BanAttribute extends PolicyManagementCLI {
 
         String resource = null;
         String action = null;
-        
+
         if (commandLine.hasOption(OPT_RESOURCE)) {
             resource = commandLine.getOptionValue(OPT_RESOURCE);
         } else {
             resource = "*";
         }
-        
+
         if (commandLine.hasOption(OPT_ACTION)) {
             action = commandLine.getOptionValue(OPT_ACTION);
         } else {
@@ -109,4 +120,3 @@ public class BanAttribute extends PolicyManagementCLI {
     }
 
 }
-
