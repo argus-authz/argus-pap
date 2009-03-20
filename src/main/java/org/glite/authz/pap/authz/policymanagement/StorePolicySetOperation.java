36b65c4e87613d3fb7046e1ac9ef6f90760637fd
Alberto Forti <alberto.forti@cnaf.infn.it>
2009-03-20 11:14:12 +0000

    Generate PolicySetId server side.

--- src/main/java/org/glite/authz/pap/authz/policymanagement/StorePolicySetOperation.java
+++ src/main/java/org/glite/authz/pap/authz/policymanagement/StorePolicySetOperation.java
@@ -5,6 +5,7 @@ import org.glite.authz.pap.authz.PAPPermission;
 import org.glite.authz.pap.authz.PAPPermission.PermissionFlags;
 import org.glite.authz.pap.common.PAP;
 import org.glite.authz.pap.common.xacml.TypeStringUtils;
+import org.glite.authz.pap.common.xacml.wizard.WizardUtils;
 import org.glite.authz.pap.distribution.PAPManager;
 import org.glite.authz.pap.repository.PAPContainer;
 import org.glite.authz.pap.services.XACMLPolicyManagementServiceException;
@@ -43,7 +44,8 @@ public class StorePolicySetOperation extends BasePAPOperation<String> {
 
         PAPContainer papContainer = new PAPContainer(pap);
 
-        String policySetId = policySet.getPolicySetId();
+        String policySetId = WizardUtils.generateId(idPrefix); 
+    	policySet.setPolicySetId(policySetId);
 
         papContainer.storePolicySet(policySet);
 
