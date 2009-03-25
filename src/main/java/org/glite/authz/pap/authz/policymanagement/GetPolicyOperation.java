35d603a5cc43183a3ddc0aee2bdd02bea614e60f
Alberto Forti <alberto.forti@cnaf.infn.it>
2009-03-25 14:29:30 +0000

    Bux fix in case of papAlias null

--- src/main/java/org/glite/authz/pap/authz/policymanagement/GetPolicyOperation.java
+++ src/main/java/org/glite/authz/pap/authz/policymanagement/GetPolicyOperation.java
@@ -3,6 +3,7 @@ package org.glite.authz.pap.authz.policymanagement;
 import org.glite.authz.pap.authz.BasePAPOperation;
 import org.glite.authz.pap.authz.PAPPermission;
 import org.glite.authz.pap.authz.PAPPermission.PermissionFlags;
+import org.glite.authz.pap.common.PAP;
 import org.glite.authz.pap.distribution.PAPManager;
 import org.glite.authz.pap.repository.PAPContainer;
 import org.glite.authz.pap.repository.exceptions.NotFoundException;
@@ -28,6 +29,10 @@ public class GetPolicyOperation extends BasePAPOperation <PolicyType>{
     @Override
     protected PolicyType doExecute() {
 
+    	if (alias == null) {
+            alias = PAP.DEFAULT_PAP_ALIAS;
+        }
+    	
         PAPContainer papContainer = PAPManager.getInstance().getPAPContainer(alias);
         
         if (!papContainer.hasPolicy(policyId)) {
--- src/main/java/org/glite/authz/pap/authz/policymanagement/GetPolicySetOperation.java
+++ src/main/java/org/glite/authz/pap/authz/policymanagement/GetPolicySetOperation.java
@@ -3,6 +3,7 @@ package org.glite.authz.pap.authz.policymanagement;
 import org.glite.authz.pap.authz.BasePAPOperation;
 import org.glite.authz.pap.authz.PAPPermission;
 import org.glite.authz.pap.authz.PAPPermission.PermissionFlags;
+import org.glite.authz.pap.common.PAP;
 import org.glite.authz.pap.distribution.PAPManager;
 import org.glite.authz.pap.repository.PAPContainer;
 import org.glite.authz.pap.repository.exceptions.NotFoundException;
@@ -29,6 +30,10 @@ public class GetPolicySetOperation extends BasePAPOperation <PolicySetType>{
     
     @Override
     protected PolicySetType doExecute() {
+    	
+    	if (alias == null) {
+            alias = PAP.DEFAULT_PAP_ALIAS;
+        }
 
         PAPContainer papContainer = PAPManager.getInstance().getPAPContainer(alias);
         
